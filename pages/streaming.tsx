cc86dcdd9403a5edc2629e8fd709aac0095482fe
MaSungHo <tonem123@naver.com>
2023-04-14 12:07:40 +0900

    fix video url

--- pages/streaming.tsx
+++ pages/streaming.tsx
@@ -2,13 +2,10 @@ import Head from 'next/head';
 import VideoPlayer from '../components/video-player';
 
 export default function Streaming() {
-    const videoUrl = "http://54.180.86.15:8088/hls/marv-live.m3u8";
+    const videoUrl = "https://marouter.site/hls/marv-live.m3u8";
 
     return (
         <div>
-            <Head>
-                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-            </Head>
             <h2>BJ 마브 첫방송 ~</h2>
             <VideoPlayer src={videoUrl}></VideoPlayer>
 
